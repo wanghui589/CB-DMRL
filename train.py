3a46dc37c213616053793fe12db0046a50191c28
Nice_hui <49338402+wanghui589@users.noreply.github.com>
2022-10-09 19:32:21 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import wandb
+import socket
+import setproctitle
+import numpy as np
+from pathlib import Path
+import torch
+from config import get_config
+from envs.env_wrappers import SubprocVecEnv, DummyVecEnv
+
+"""Train script for PCS."""
+
+
+waiting for impout
